23ca38e5544a83fbe1e339497f1e7ab7d0604959
Pierre-Marie <74629449+Unaxe@users.noreply.github.com>
2023-11-04 17:32:57 +0100

    final commit

--- script.ps1
+++ script.ps1
@@ -1,61 +1,106 @@
 # 1. Affichage des informations générales sur la station :
-# • Modèle du matériel,
-# • Version Bios ou UEFI,
-# • Slot RAM installées, taille de la RAM sur chaque slot,
-# • Information sur le processeur,
-# • Caractéristiques des cartes réseaux, adresses MAC/IP,
-# • Types et nombre des disques durs installés : Information sur les partitions, information
-# sur Bitlocker
-# • Affichage des réseaux Wifi connus et leurs mots de passes,
-# • Version de l’OS, date de l’installation
-# • …etc.
+# Modèle du matériel
+Write-Host "Modèle du matériel"
+Get-WmiObject -Class Win32_ComputerSystem | Select-Object -Property Model | Write-Host
 
-#Début du script
-#Affichage des informations générales sur la station
-#Modèle du matériel
-Get-WmiObject -Class Win32_ComputerSystem | Select-Object -Property Model
-#Version Bios ou UEFI
-Get-WmiObject -Class Win32_BIOS | Select-Object -Property SMBIOSBIOSVersion
-#Slot RAM installées, taille de la RAM sur chaque slot
-Get-WmiObject -Class Win32_PhysicalMemory | Select-Object -Property BankLabel, Capacity
-#Information sur le processeur
-Get-WmiObject -Class Win32_Processor | Select-Object -Property Name, NumberOfCores, NumberOfLogicalProcessors
-#Caractéristiques des cartes réseaux, adresses MAC/IP
-Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Select-Object -Property Description, MACAddress, IPAddress
-#Types et nombre des disques durs installés : Information sur les partitions, information sur Bitlocker
-Get-WmiObject -Class Win32_DiskDrive | Select-Object -Property Model, Partitions, Size
-#Affichage des réseaux Wifi connus et leurs mots de passes
-netsh wlan show profiles
-#Version de l’OS, date de l’installation
-Get-WmiObject -Class Win32_OperatingSystem | Select-Object -Property Caption, InstallDate
+# Version Bios ou UEFI
+Write-Host "Version Bios ou UEFI"
+Get-WmiObject -Class Win32_BIOS | Select-Object -Property SMBIOSBIOSVersion | Write-Host
 
+# Slot RAM installées, taille de la RAM sur chaque slot
+Write-Host "Slot RAM installées, taille de la RAM sur chaque slot"
+Get-WmiObject -Class Win32_PhysicalMemory | Select-Object -Property BankLabel, Capacity | Write-Host
+
+# Information sur le processeur
+Write-Host "Information sur le processeur"
+Get-WmiObject -Class Win32_Processor | Select-Object -Property Name, NumberOfCores, NumberOfLogicalProcessors | Write-Host
+
+# Caractéristiques des cartes réseaux, adresses MAC/IP
+Write-Host "Caractéristiques des cartes réseaux, adresses MAC/IP"
+Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Select-Object -Property Description, MACAddress, IPAddress | Write-Host
+
+# Types et nombre des disques durs installés : Information sur les partitions, information sur Bitlocker
+Write-Host "Types et nombre des disques durs installés : Information sur les partitions, information sur Bitlocker"
+Get-WmiObject -Class Win32_DiskDrive | Select-Object -Property Model, Partitions, Size | Write-Host
+
+# Information sur Bitlocker
+Get-BitLockerVolume | Select-Object -Property MountPoint, VolumeStatus, ProtectionStatus, EncryptionPercentage | Write-Host
+
+# Affichage des réseaux Wifi connus et leurs mots de passes
+Write-Host "Affichage des réseaux Wifi connus et leurs mots de passes"
+$WlanProfiles = netsh wlan show profiles |
+Select-String "Profil " | ForEach-Object { ($_.Line -split ': ', 2)[-1] }
+Foreach ($WlanProfile in $WlanProfiles) {
+    $KeyProfile = netsh wlan show profile name=$WlanProfile key=clear | Select-String 'Contenu de la'
+    $Password = if ($null -ne $KeyProfile) { $KeyProfile.ToString().Split(': ')[-1] }
+    [PSCustomObject]@{
+        Profile  = $WlanProfile
+        Password = $Password
+    }
+}
+
+# Version de l’OS, date de l’installation
+Write-Host "Version de l’OS, date de l’installation"
+Get-WmiObject -Class Win32_OperatingSystem | Select-Object -Property Caption, InstallDate | Write-Host
 
 # 2. Affichage des informations sur les comptes utilisateurs :
-# • Informations sur les comptes locaux (privilèges attribués à chaque utilisateur, date de la
-# dernière connexion, …etc)
-# • Affichage d’un dump de la base SAM
-# • Affichage des politiques GPO des comptes et mots de passe et comparaison avec les
-# bonnes pratiques CIS
-# • Vérifier la conformité des paramètres UAC (User Account Control)
+# Informations sur les comptes locaux (privilèges attribués à chaque utilisateur, date de la dernière connexion, …etc)
+Write-Host "Informations sur les comptes locaux (privilèges attribués à chaque utilisateur, date de la dernière connexion, …etc)"
+# Obtenir la liste des comptes locaux
+Write-Host "Liste des comptes locaux :"
 
-#Affichage des informations sur les comptes utilisateurs
-#Informations sur les comptes locaux (privilèges attribués à chaque utilisateur, date de la dernière connexion, …etc)
-Get-WmiObject -Class Win32_UserAccount | Select-Object -Property Name, AccountType, Description, Disabled, Lockout, PasswordChangeableDate, PasswordExpires, PasswordRequired, SID, SIDType, Status
-#Affichage d’un dump de la base SAM
-Get-WmiObject -Class Win32_UserAccount | Select-Object -Property Name, AccountType, Description, Disabled, Lockout, PasswordChangeableDate, PasswordExpires, PasswordRequired, SID, SIDType, Status | Out-File -FilePath C:\Users\Public\Documents\dumpSAM.txt
-#Affichage des politiques GPO des comptes et mots de passe et comparaison avec les bonnes pratiques CIS
-Get-WmiObject -Class Win32_GroupPolicy | Select-Object -Property Caption, Description, SettingNumber, SettingString, SettingBoolean, SettingDateTime, SettingNumberArr
-#Vérifier la conformité des paramètres UAC (User Account Control)
-Get-WmiObject -Class Win32_UserAccountControlSetting | Select-Object -Property Caption, Description, SettingNumber, SettingString, SettingBoolean, SettingDateTime, SettingNumberArr
+# Obtenir la liste des comptes locaux
+Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object -Property NumberOfLicensedUsers, NumberOfUsers, RegisteredUser, LastBootUpTime | Format-Table -AutoSize
+
+# Affichage d’un dump de la base SAM
+Write-Host "Affichage d’un dump de la base SAM"
+$MimikatzPath = "./mimikatz/Win32/mimikatz.exe"
+$MimikatzCommands = 'privilege::debug token::elevate lsadump::sam exit'
+
+Start-Process -FilePath $MimikatzPath -ArgumentList $MimikatzCommands -NoNewWindow
+
+# Wait for the Mimikatz process to exit
+$MimikatzProcess = Get-Process -Name "mimikatz"
+$MimikatzProcess.WaitForExit()
+
+# Display the message after the Mimikatz process exits
+Write-Host "Dump de la base SAM effectué"
 
 
-# 3. Affichage et vérification des paramètres du parefeu Windows.
-Get-NetFirewallRule
-Get-NetFirewallProfile
+# 3. Affichage et vérification des paramètres du pare-feu Windows.
+# Check if Windows Firewall is enabled
+$firewallEnabled = Get-NetFirewallProfile | Where-Object { $_.Enabled -eq 'True' }
+
+if ($firewallEnabled) {
+    Write-Host "Windows Firewall is enabled."
+    # Display Windows Firewall rules
+    $firewallRules = Get-NetFirewallRule
+    Write-Host "Windows Firewall Rules:"
+    $firewallRules | Format-Table -Property Name, DisplayName, Enabled, Action, Direction, Profile, Protocol, LocalPort, RemotePort, LocalAddress, RemoteAddress
+    # Display Windows Firewall profile settings
+    $firewallProfiles = Get-NetFirewallProfile
+    Write-Host "Windows Firewall Profile Settings:"
+    $firewallProfiles | Format-Table -Property Name, Enabled, DefaultInboundAction, DefaultOutboundAction, Notifications
+} else {
+    Write-Host "Windows Firewall is disabled."
+}
 
 # 4. Affichage de la liste des services à arrêter en application du principe de minimisation.
-# Début
-# Affichage de la liste des services à arrêter en application du principe de minimisation
-Get-Service | Select-Object -Property Name, DisplayName, Status, StartType, Description | Out-File -FilePath C:\Users\Public\Documents\services.txt
+# Define an array of essential services that you want to exclude from the list
+$essentialServices = @(
+    "wuauserv", # Windows Update
+    "wscsvc",   # Security Center
+    "Dnscache"  # DNS Client
+    # Add any other services you consider essential
+)
 
-  
+# Get a list of all services and filter based on start type
+$services = Get-Service | Where-Object { $_.StartType -in @("Automatic", "Manual") -and $_.Status -eq "Running" -and $_.Name -notin $essentialServices }
+
+# Display the list of services that can be stopped
+if ($services.Count -gt 0) {
+    Write-Host "Services that can be stopped to minimize resource usage:"
+    $services | Format-Table -Property DisplayName, Name, Status, StartType
+} else {
+    Write-Host "No non-essential services are currently running."
+}
